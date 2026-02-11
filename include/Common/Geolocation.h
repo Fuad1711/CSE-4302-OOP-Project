0bdf144b80267f81723536568d62d046a4ba7c25
Fuad1711 <ff.fuad01@gmail.com>
2026-02-11 14:08:01 +0600

    Add GeoLocation class with distance calculation

---
+++ include/Common/Geolocation.h
@@ -0,0 +1,32 @@
+#ifndef GEOLOCATION_H
+#define GEOLOCATION_H
+#include<math.h>
+
+class GeoLocation{
+private:
+    double latitude;
+    double longitude;
+public:
+    GeoLocation() : latitude(0.0), longitude(0.0) {}
+    GeoLocation(double lat, double lon) : latitude(lat), longitude(lon) {}
+
+    double  getLatitude() const{
+        return latitude;
+    }
+    double getlongtitude() const{
+        return longitude;
+    }
+    void setLatitude(double lat){
+        latitude = lat;
+    }
+    void setlongtitude(double lon){
+        longitude = lon;
+    }
+    double calculateDistance(const GeoLocation& location1, const GeoLocation& location2){
+        double x = location1.latitude - location2.latitude;
+        double y = location1.longitude - location2.longitude;
+        return sqrt(pow(x, 2) + pow(y, 2));
+    }
+};
+
+#endif
