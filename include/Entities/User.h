f74163fa29d968f1511317506f7a0863eedc016e
Kornflaek14 <aymannazir@iut-dhaka.edu>
2026-03-06 00:11:30 +0600

    Did sam wark

--- include/Entities/User.h
+++ include/Entities/User.h
@@ -7,36 +7,36 @@
 using namespace std;
 
 class User{
-    protected:
+protected:
 
-        string firstName;
-        string lastName;
-        string phoneNumber;
-        string homeAddress;
-        string email;
-        string userName;
-        string password;
+    string firstName;
+    string lastName;
+    string phoneNumber;
+    string homeAddress;
+    string email;
+    string userName;
+    string password;
+    //Date of Birth
+    int day,month,year;
+    int age;
 
-        //Date of Birth
-        int day,month,year;
-        int age;
-    public:
+public:
 
-        User():firstName(),lastName(),phoneNumber(),homeAddress(),email(),userName(),password(),day(),month(),year(){}
-        User(string firstName,string lastName, string phoneNumber,string address, int day, int month, int year, string email,string uname, string password):
-        firstName(firstName),
-        lastName(lastName),
-        phoneNumber(phoneNumber),
-        homeAddress(address),
-        day(day),
-        month(month),
-        year(year),
-        email(email),
-        userName(uname),
-        password(password){}
-        void updateProfile();
-        void viewProfile();
-        void saveData();
+    User():firstName(""),lastName(""),phoneNumber(""),homeAddress(""),email(""),userName(""),password(""),day(0),month(0),year(0){}
+    
+    User(string firstName,string lastName, string phoneNumber,string address, int day, int month, int year, string email,string uname, string password):
+    firstName(firstName),
+    lastName(lastName),
+    phoneNumber(phoneNumber),
+    homeAddress(address),
+    day(day), month(month), year(year),
+    email(email),
+    userName(uname),
+    password(password){}
+
+    void updateProfile();
+    virtual void viewProfile()=0;
+    void saveData();
 };
 
 #endif
